67b796a8af0ccb9161164eda322c9c98cb88aab5
João Nisa <joao.je.nisa@gmail.com>
2022-01-28 17:18:28 +0000

    completness of the sqlite interaction methods

--- tdd_stage/app/engine/python/sqlite/methods.py
+++ tdd_stage/app/engine/python/sqlite/methods.py
@@ -1,6 +1,23 @@
 
 
 
+def data_prep(vals_set: list):
+
+    '''
+    prepares data before making the insert into a new dimension
+
+    :param vals_set: set of values that are about to be reshaped
+    :param cols_set: set of column that will indicate the number of divisions
+    '''
+
+    vals_struct = [[] for _ in range(len(vals_set[0]))]
+    
+    for rec in vals_set:
+        for idx, val in enumerate(rec):
+            vals_struct[idx].append(val)
+
+    return vals_struct
+
 
 def create_table(table_id: str, cols_set: list, dtypes: list):
 
@@ -31,3 +48,34 @@ def data_load(table_id: str, cols_set: list):
     query = "INSERT INTO %s %s VALUES %s" %(table_id, tuple(cols_set), str(vals_spots))
 
     return query.replace("'", "")
+
+
+def sql_join(tab1: str, tab2: str, c1: str, c2: str, join_type: str, new_table: str):
+
+    '''
+    performs a join between two dimensions
+
+    :param tab1: first table of the join operation
+    :param tab2: second table of the join operation
+    :param c1: column that will be targeted on the join from 1st table
+    :param c2: column that will be targeted on the join from 2nd table
+    :param join_type: type of join to be performed
+    :param new_table: outcome table from the select command
+    '''
+
+    join_map = {
+        'left': 'LEFT JOIN',
+        'inner': 'INNER JOIN',
+        'left outer': 'LEFT OUTER JOIN',
+        'cross': 'CROSS JOIN'
+    }
+
+    attr1 = '.'.join([tab1, c1])
+    attr2 = '.'.join([tab2, c2])
+
+    query = "CREATE TABLE %s AS SELECT * FROM %s %s %s ON %s = %s" %(
+        new_table, tab1, join_map[join_type], tab2, attr1, attr2 
+    ) 
+
+    return query.replace("'", "")
+
--- tdd_stage/tests/unit/engine/python/test_sqlite/test_methods.py
+++ tdd_stage/tests/unit/engine/python/test_sqlite/test_methods.py
@@ -3,11 +3,32 @@
 
 import unittest
 
-from tdd_stage.app.engine.python.sqlite.methods import create_table
+from tdd_stage.app.engine.python.sqlite.methods import sql_join
 from tdd_stage.app.engine.python.sqlite.methods import data_load
+from tdd_stage.app.engine.python.sqlite.methods import data_prep
+from tdd_stage.app.engine.python.sqlite.methods import create_table
 
 
 class TestSQLiteMethods(unittest.TestCase):
+
+    def test_data_prep_tc1(self):
+
+        '''
+        data_prep - 1st Test Case Scenario
+        Complexity: 1/4
+        '''
+
+        vals_set = [
+            ['abc', 1, 'def'], 
+            ['ghi', 2, 'jkl'], 
+            ['mno', 3, 'pqr']
+        ]
+
+        result = data_prep(vals_set)
+        expected = [['abc', 'ghi', 'mno'], [1, 2, 3], ['def', 'jkl', 'pqr']]
+
+        return self.assertEqual(result, expected)
+
 
     def test_create_table_tc1(self):
 
@@ -38,5 +59,18 @@ class TestSQLiteMethods(unittest.TestCase):
 
         expected = "INSERT INTO test_table_1 (column_1, column_2, column_3) VALUES (?, ?, ?)" 
         result = data_load(table_id, cols_set)
+
+        return self.assertEqual(result, expected)
+
+
+    def test_sql_join_tc1(self):
+
+        '''
+        sql_join - 1st Test Case Scenario
+        Complexity: 1/4
+        '''
+
+        result = sql_join('table1', 'table2', 'id', 'id', 'left', 'new_table_1')
+        expected = "CREATE TABLE new_table_1 AS SELECT * FROM table1 LEFT JOIN table2 ON table1.id = table2.id"
 
         return self.assertEqual(result, expected)
